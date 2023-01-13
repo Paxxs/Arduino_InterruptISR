c34ca6da09b7efcda962bdff9f3c14ac90eb54c6
Paxxs <10364775+Paxxs@users.noreply.github.com>
2023-01-13 23:02:23 +0800

    更换一种方式跳过抖动时间

--- attachInterrupt_led_btn/ledtoggle.ino
+++ attachInterrupt_led_btn/ledtoggle.ino
@@ -1,12 +1,13 @@
-
 // 定义要使用的引脚
 #define LED_PIN 9
 #define BTN_PIN 2
 
-// 抖动计算
-float pre_btn_time_2;
 volatile bool btn_state_2 = 0;
-#define debounce 100 // time to wait in milli secs
+volatile unsigned int btn_count;
+
+// 抖动计算
+volatile unsigned long ignore_time;
+#define debounce 250 // time to wait in milli secs
 
 void setup()
 {
@@ -25,33 +26,33 @@ void setup()
 
 void loop()
 {
-    Serial.println(F("Delay 3s"));
+    Serial.println(F("Delay 1s"));
     Serial.print(F(" "));
     Serial.print("Btn:");
     Serial.print(digitalRead(BTN_PIN));
     Serial.print(F("-"));
     Serial.print(btn_state_2);
+    Serial.print(F("-"));
+    Serial.print(btn_count);
     Serial.print(F(" "));
-    Serial.println(pre_btn_time_2);
-    delay(3000);
+    Serial.print(ignore_time);
+    Serial.print(F(" "));
+    Serial.println(millis());
     Serial.println("----------");
+    delay(1000);
 }
 
 void CheckBtnIsr()
 {
-    cli();
     // ISR 中使用 millis() 函数获取到的值不会改变
     // ISR 函数，更改 led 状态
+    if (ignore_time > millis()) // 跳过抖动时间
+        return;
     if (digitalRead(BTN_PIN) == HIGH)
     {
-        if (btn_state_2 == 0) // 按钮为关
-        {
-            pre_btn_time_2 = millis(); // 重置计时器
-            btn_state_2 = 1;
-        }
-        else if (btn_state_2 == 1 && (millis() - pre_btn_time_2 > debounce))
-            btn_state_2 = 0;
+        ignore_time = millis() + debounce; // reset timer
+        btn_state_2 = !btn_state_2;
         digitalWrite(LED_PIN, btn_state_2);
+        btn_count++;
     }
-    sei();
 }
